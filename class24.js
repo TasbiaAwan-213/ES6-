704934f51e72d6fa3afb65501913ee9f896d72aa
tasbiaawan <58261582+TasbiaAwan-213@users.noreply.github.com>
2020-11-16 20:23:16 +0500

    Update class24.js

--- class24.js
+++ class24.js
@@ -253,8 +253,22 @@ let multiply = arr.map(a => {
    })
 console.log(multiply)
 
+           
+           ******//////fetch*/* ********   
+          
+           //*database (.than or .catch 'keywords')//*
 
+fetch ('https//jsonplaceholder.typicode.com/todo/1')
+.then(response => response.json())           
+.then(data => console.log(data))
+.catch(err => console.log(err))
+           
+           
+ **/// for dataget key words ( async 'await')
+async function getData(){
+  let data = await fetch ('https//jsonplaceholder.typicode.com/todo/1')
+    console.log(data)
+}
 
-
-
-
+        *****//ES6 classes *//
+//ex:- ES5 constracter function         
